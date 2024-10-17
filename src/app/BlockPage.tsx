a6466eeec700a23cee4859dad9a40070fda3aced
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2024-10-17 15:13:52 +0700

    fix error next image

--- src/app/BlockPage.tsx
+++ src/app/BlockPage.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import React from 'react'
 
 const BlockPage = () => {
@@ -35,7 +34,7 @@ const BlockPage = () => {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">        
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://dhanav.me"
           target="_blank"
           rel="noopener noreferrer"
         >
