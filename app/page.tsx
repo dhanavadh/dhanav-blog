7fc8270d02ae678351163609f4b4c10aef3ea2e4
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2024-11-25 23:19:36 +0700

    add legacy link

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
         </a> 
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/docs"
+          href="https://legacy.dhanav.me/"
           target="_blank"
           rel="noopener noreferrer"
         >
