5505ba688efffa24208139faf94d2a94d1b54aa3
Dhanavadh T. Saito <saito.teruyuki@outlook.jp>
2025-01-21 21:43:20 +0700

    deprecated page alert

--- app/components/ProfileInfo.tsx
+++ app/components/ProfileInfo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ProfileInfo = () => {
   return (
     <div className='grid min-h-40 max-w-5xl items-center justify-items-start sm:justify-center font-[family-name:var(--font-noto-sans)]'>
-        <div className='mt-8 sm:mt-16 mx-10 sm:mx-40'>
+        <div className='mx-10 sm:mx-40'>
           <p className='heading'>Dhanavadh Saito</p>
           <p className='sub-heading mt-4'>ธนวัฒน์ ซาอิโตะ</p>                  
           <p className='paragraph mt-2'>Nothing too much here but, I am a 3rd-year student currently majoring in Japanese language at the Faculty of Arts, Chulalongkorn University.</p>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ import Projects from "./components/Projects";
 
 export default function Home() {
   return (
-    <div className="bg-[#f2f2f2]">
+    <div className="bg-[#f2f2f2] flex flex-col items-start">
+      <div className='bg-white mt-12 mb-12 mx-10 sm:mx-40 border-l-8 border-red-500 px-4 py-2'> 
+          <p className='text-xl font-bold text-red-700'>Deprecated Page</p>          
+          <p className='text-lg'>This page is deprecated. To see the new ones, please visit <a href="https://dhanav.me/" target="_blank" className="hover:underline underline-offset-2">https://dhanav.me/</a></p>          
+      </div>  
       <ProfileInfo />    
       <div className='mt-6 mx-10 mb-2 sm:mx-40'>
           <p className='h1'>Projects</p>          
