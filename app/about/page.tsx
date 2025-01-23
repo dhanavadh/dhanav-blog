4a2108ef4f4c3a94330a06b65a1021de09119ac7
Dhanavadh T. Saito <saito.teruyuki@outlook.jp>
2025-01-23 12:29:32 +0700

    add resume

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -39,53 +39,77 @@ const AboutMe = () => {
                     <Bio />
 
                     {/* Contact */}
-                    <p className='h3 mt-6 mb-2'>Reach me via</p>
-                    <div className='flex flex-col gap-1'>                    
-                        <Link
-                        className="flex items-center gap-2 link"
-                        href="mailto:dhanavadh@gmail.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >
-                        <Image
-                            aria-hidden
-                            src="icons/mail.svg"
-                            alt="mail"
-                            width={16}
-                            height={16}
-                        />
-                        dhanavadh@gmail.com
-                        </Link>
-                        <Link
-                        className="flex items-center gap-2 link"
-                        href="https://github.com/dhanavadh"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >
-                        <Image
-                            aria-hidden
-                            src="/github-mark.svg"
-                            alt="GitHub icon"
-                            width={16}
-                            height={16}
-                        />
-                        @dhanavadh
-                        </Link>
-                        <Link
-                        className="flex items-center gap-2 link"
-                        href="https://www.linkedin.com/in/dhanav/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >
-                        <Image
-                            aria-hidden
-                            src="/ln.svg"
-                            alt="LinkedIn icon"
-                            width={16}
-                            height={16}
-                        />
-                        @dhanav
-                        </Link>
+                    <div className='flex gap-4 flex-col md:flex-row'>
+                        <div>
+                            <p className='h3 mt-6 mb-2'>Reach me via</p>
+                            <div className='flex flex-col gap-1'>                    
+                                <Link
+                                className="flex items-center gap-2 link"
+                                href="mailto:dhanavadh@gmail.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <Image
+                                    aria-hidden
+                                    src="icons/mail.svg"
+                                    alt="mail"
+                                    width={16}
+                                    height={16}
+                                />
+                                dhanavadh@gmail.com
+                                </Link>
+                                <Link
+                                className="flex items-center gap-2 link"
+                                href="https://github.com/dhanavadh"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <Image
+                                    aria-hidden
+                                    src="/github-mark.svg"
+                                    alt="GitHub icon"
+                                    width={16}
+                                    height={16}
+                                />
+                                @dhanavadh
+                                </Link>
+                                <Link
+                                className="flex items-center gap-2 link"
+                                href="https://www.linkedin.com/in/dhanav/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <Image
+                                    aria-hidden
+                                    src="/ln.svg"
+                                    alt="LinkedIn icon"
+                                    width={16}
+                                    height={16}
+                                />
+                                @dhanav
+                                </Link>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='h3 mt-6 mb-2'>Attachements</p>
+                            <div className='flex flex-col gap-1'>                    
+                                <Link
+                                className="flex items-center gap-2 link"
+                                href="/asset/Resume-2025.pdf"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                >
+                                <Image
+                                    aria-hidden
+                                    src="icons/read.svg"
+                                    alt="mail"
+                                    width={16}
+                                    height={16}
+                                />
+                                Resume
+                                </Link>
+                            </div>
+                        </div>
                     </div>
             </div>
             {/* Skills */}
