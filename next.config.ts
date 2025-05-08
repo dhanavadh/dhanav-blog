7f501f21b89c3a6a6dbe267c67774644d119c420
Dhanavadh T. Saito <saito.teruyuki@outlook.jp>
2025-05-08 21:07:36 +0700

    update cf deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
-
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 module.exports = {
   typescript: {
     // !! WARN !!
